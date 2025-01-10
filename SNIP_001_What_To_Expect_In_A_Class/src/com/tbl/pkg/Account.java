5eb87813c4b9a2008d4181a11e68402144ab9496
aritchat <aritrachatterjee2007@gmail.com>
2025-01-10 22:11:25 +0530

    prob(SNIP_001): Adding basic elements of a class declaration

---
+++ SNIP_001_What_To_Expect_In_A_Class/src/com/tbl/pkg/Account.java
@@ -0,0 +1,41 @@
+package com.tbl.pkg;
+
+public class Account { //Class name
+    //Class Declarations:
+    //Fields:
+    private String accountType;
+    private String accountId;
+
+    //Constructor:
+    public Account(String accountId, String accountType){
+        this.accountType = accountType;
+        this.accountId = accountId;
+    }
+
+    //Methods:
+    @Override
+    //Instance Methods
+    public String toString(){
+        return "Overridden toString method called: com.tbl.pkg.Account type: " + accountType + ", com.tbl.pkg.Account ID: " + accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    //Static methods
+    public static void main(String[] args) {
+        Account newAccount = new Account("CR0012", "Current");
+
+        System.out.println("Original account details: " + newAccount);
+
+        newAccount.setAccountId("CR0020");
+        newAccount.setAccountType("Savings");
+
+        System.out.println("Updated account details: " + newAccount);
+    }
+}
