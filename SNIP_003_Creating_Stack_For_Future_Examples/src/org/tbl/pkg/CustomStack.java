5bff92a52b651901abfb6da8099ca0338353bf92
aritchat <aritrachatterjee2007@gmail.com>
2025-01-11 09:11:58 +0530

    prob(SNIP_003): Creating a custom stack for integers

---
+++ SNIP_003_Creating_Stack_For_Future_Examples/src/org/tbl/pkg/CustomStack.java
@@ -0,0 +1,34 @@
+package org.tbl.pkg;
+
+public class CustomStack {
+    //Instance variables
+    int[] elements; //We will keep integers in this stack
+    int stackTop; //This is the pointer to the stack top
+
+    //Constructor
+    public CustomStack(int capacity){
+        elements = new int[capacity]; //defining the stack with its size
+        stackTop = -1; //This is use to indicate the stack is empty
+    }
+
+    //Operations using instance methods
+    public void push(int element){
+        elements[stackTop++] = element; //Adding element at the current stack top
+    }
+
+    public int pop(){
+        return elements[stackTop--]; //removing the element at the current stack top
+    }
+
+    public int peek(){
+        return elements[stackTop]; //Returns the element at the current stack top
+    }
+
+    public boolean isEmpty(){
+        return stackTop < 0; //if stackTop is < 0, it means the array is empty
+    }
+
+    public boolean isFull(){
+        return stackTop == elements.length;
+    }
+}
