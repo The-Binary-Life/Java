37aba7cf96e27202492247c68c45260a8600a7d4
aritchat <aritrachatterjee2007@gmail.com>
2025-01-14 10:17:53 +0530

    prob(SNIP_006): Creates a java bean

---
+++ SNIP_006_Its_Bean_A_Pleasure/src/org/tbl/pkg/JavaBean.java
@@ -0,0 +1,41 @@
+package org.tbl.pkg;
+
+import java.io.Serializable;
+
+//The class should implement Serializable marker interface
+public class JavaBean implements Serializable {
+    //Properties should be private
+    private String alertMessage;
+    private String warningIcon;
+    private boolean showCancelButton;
+
+    //Should contain a no-arg default constructor
+
+    //Should contain setters to set data in the private members
+
+    public void setAlertMessage(String alertMessage) {
+        this.alertMessage = alertMessage;
+    }
+
+    public void setWarningIcon(String warningIcon) {
+        this.warningIcon = warningIcon;
+    }
+
+    public void setShowCancelButton(boolean showCancelButton) {
+        this.showCancelButton = showCancelButton;
+    }
+
+    //Should contain getters to get the data from the private members
+
+    public String getAlertMessage() {
+        return alertMessage;
+    }
+
+    public String getWarningIcon() {
+        return warningIcon;
+    }
+
+    public boolean isShowCancelButton() {
+        return showCancelButton;
+    }
+}
